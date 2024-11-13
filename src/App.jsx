9991afda0dd5dc814f7e0ecab032549c445fffb6
AdliXSec <naufalsyahruradli@gmail.com>
2024-11-13 15:06:43 +0700

    first react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Card from "./Components/Elements/card";
-import FormLogin from "./Components/FormLogin";
+import FormRegistrasi from "./Components/FormRegistrasi";
 import FormPesan from "./Components/FormPesan";
 
 function App() {
@@ -16,11 +16,11 @@ function App() {
       </div>
       <div className="flex justify-center min-h-screen items-center">
         <div className="w-full max-w-xs">
-          <h1 className="text-3xl font-bold mb-2 text-blue-600">Login</h1>
+          <h1 className="text-3xl font-bold mb-2 text-blue-600">Registrasi</h1>
           <p className="font-medium text-slate-500 mb-8">
             Enter your email and password
           </p>
-          <FormLogin />
+          <FormRegistrasi />
         </div>
       </div>
     </>
---
+++ src/Components/FormRegistrasi.jsx
@@ -0,0 +1,31 @@
+import FormInput from "./Elements/Input";
+import Button from "./Elements/Button";
+
+const FormRegistrasi = () => {
+  return (
+    <form action="">
+      <FormInput type="email" placeholder="email" name="email" label="email" />
+      <FormInput
+        type="text"
+        placeholder="Username"
+        name="username"
+        label="username"
+      />
+      <FormInput
+        type="password"
+        placeholder="Password"
+        name="password"
+        label="password"
+      />
+      <FormInput
+        type="password"
+        placeholder="Password"
+        name="password"
+        label="password"
+      />
+      <Button variant="bg-blue-700">Login</Button>
+    </form>
+  );
+};
+
+export default FormRegistrasi;
